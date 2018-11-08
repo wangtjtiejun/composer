687cddec736beee5fade76a80b64993fe660d1e7
wangtjtiejun <13241701929@163.com>
2018-11-08 11:10:51 +0800

    ~

---
+++ wtj/test-composer/curl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangtiejun
+ * Date: 2018/11/8
+ * Time: 10:53
+ */
+
+class curl
+{
+    public static function helloword()
+    {
+        return "hello word";
+    }
+}
